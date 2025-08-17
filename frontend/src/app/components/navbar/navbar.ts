28fd48559be8d39380f081eee146ed39be1660a9
Centowsky <centowsky@gmail.com>
2025-08-18 00:09:16 +0200

    Create navbar options with routeLinks

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss'
+  styleUrl: './navbar.scss',
 })
 export class Navbar {
+  linksAuth = [
+    { label: 'Dashboard', path: '/dashboard' },
+    { label: 'Transactions', path: '/transactions' },
+    { label: 'Reports', path: '/reports' },
+    { label: 'Settings', path: '/settings' },
+  ];
 
+  linksGuest = [
+    { label: 'Login', path: '/login' },
+    { label: 'Register', path: '/register' },
+  ];
+
+  isAuthenticated: boolean = true;
+
+  logout() {
+    this.isAuthenticated = false;
+  }
 }
